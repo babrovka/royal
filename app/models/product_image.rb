66cd9a8d451c3f92613001f858503cb8c35f6c53
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-19 18:12:41 +0200

    deploy.rb

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,8 +1,5 @@
 class ProductImage < ActiveRecord::Base
   attr_accessible :image, :product_id
   belongs_to :product
-  has_attached_file :image, 
-                    :url  => "/system/product_images/images/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/system/product_images/images/:id/:style_:basename.:extension",
-                    :styles => { :catalog => "200x200#", :product => "300x300#", :ico => "70x70#" }
+  has_attached_file :image, :styles => { :catalog => "200x200#", :product => "300x300#", :ico => "70x70#" }
 end
