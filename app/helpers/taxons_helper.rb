e6a4bcd77adf097520e158df3bfb3ff820e827a4
Viacheslav Bobrov <babrovka@gmail.com>
2014-03-28 11:40:08 +0400

    taxon path

--- app/helpers/taxons_helper.rb
+++ app/helpers/taxons_helper.rb
@@ -2,7 +2,7 @@ module TaxonsHelper
   
   def taxon_seo_path(taxon)
     if taxon.taxonomy
-      taxon_path(taxon.taxonomy.id, taxon)
+      taxon_path(taxon.taxonomy.slug, taxon)
     else
       taxon_path('no_taxonomy', taxon)
     end
