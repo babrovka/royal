a69b4ccb545eb01f26903e6c7d0f5d0b48fee51c
justvitalius <justvitalius@gmail.com>
2014-05-03 16:59:37 +0400

    Render parthners list.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,29 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
+  helper_method :parthners
+
     
   def current_cart
-  if current_user
-    cart = Cart.find_or_create_by_user_id(current_user.id)
-  else
-    cart = nil
-  end
+    if current_user
+      cart = Cart.find_or_create_by_user_id(current_user.id)
+    else
+      cart = nil
+    end
     @cart = cart
   end
   
     
-    def seo_text
-      seo = SeoData.find_by_page('Главная страница')
-      @seo_text = seo.try(:seo_text) || ''
-    end
+  def seo_text
+    seo = SeoData.find_by_page('Главная страница')
+    @seo_text = seo.try(:seo_text) || ''
+  end
+
+  private
+
+  def parthners
+    Partner.all
+  end
   
 end
