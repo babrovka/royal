01e5ee38cd93488216f68d19643a8cec2fb069b1
justvitalius <justvitalius@gmail.com>
2014-05-23 13:42:58 +0400

    Add darken to case header.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ Rake::Task['db:test_taxonomies'].invoke
 Rake::Task['db:test_nested_taxons'].invoke
 Rake::Task['db:create_products'].invoke
 Rake::Task['db:create_seo_data_objects'].invoke
-Rake::Task['db:create_cities'].invoke
+# Rake::Task['db:create_cities'].invoke
 Rake::Task['db:test_events'].invoke
 Rake::Task['db:test_partners'].invoke
 Taxon.find_each(&:save!)
