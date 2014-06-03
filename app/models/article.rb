11bdc2909252895f9fd4e7bfb3213e82f7de3a18
justvitalius <justvitalius@gmail.com>
2014-06-03 19:53:56 +0400

    Add image to Article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,25 @@
 class Article < ActiveRecord::Base
-  attr_accessible :date, :text, :title, :article_images_attributes, :short_text
+  attr_accessible :date,
+                  :text,
+                  :title,
+                  :image,
+                  :article_images_attributes,
+                  :short_text
+
+
   has_many :article_images
+
   accepts_nested_attributes_for :article_images, :allow_destroy => true
-  validates :title, :date, :text, :presence => true
+
   just_define_datetime_picker :date, :add_to_attr_accessible => true
+
+  validates :title, :date, :text, :presence => true
   validates :text, :date, :title, :short_text, :presence => true
   validates :short_text, :length => { :maximum => 150 }
+
   default_scope order('date DESC')
+
+
+  has_attached_file :image, :styles => {:medium => "694x264>"}, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140528114811) do
+ActiveRecord::Schema.define(:version => 20140603154930) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -78,9 +78,13 @@ ActiveRecord::Schema.define(:version => 20140528114811) do
     t.string   "title"
     t.date     "date"
     t.text     "text"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.text     "short_text"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "brands", :force => true do |t|
