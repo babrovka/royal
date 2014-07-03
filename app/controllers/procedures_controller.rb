9450d4f3ee63c5a6bbef563fdd7c969ed59bcd48
justvitalius <justvitalius@gmail.com>
2014-07-03 22:28:17 +0400

    Render dirty procedures list.

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,12 +1,14 @@
 class ProceduresController < ApplicationController
   def index
-    @procedures = Procedure.all
+    @procedures = Procedure.order('title ASC').page(params[:page]).per_page(10)
   end
   
   def category
     category = ProcedureCategory.find(params[:id])
     categories = category.self_and_descendants
-    @procedures = Product.includes(:procedure_categories).where(:procedure_categories => {:id => categories})
+    @procedures = Product.includes(:procedure_categories)
+                          .where(:procedure_categories => {:id => categories})
+                          .page(params[:page]).per_page(10)
   end
   
   def show 
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,12 +1,13 @@
 class Procedure < ActiveRecord::Base
   attr_accessible :title, :text, :stages_attributes, :short_text, :image
+
   has_many :stages
-  accepts_nested_attributes_for :stages, :allow_destroy => true
-  has_attached_file :image, :styles => { :medium => "940x380#", :thumb => "200x100>" }
   has_many :procedure_procedure_categories
   has_many :procedure_categories, through: :procedure_procedure_categories
-  
-  
+
+  has_attached_file :image, :styles => { :medium => "940x380#", :thumb => "200x100>" }
+  accepts_nested_attributes_for :stages, :allow_destroy => true
+
   def brand 
     unless self.procedure_categories.empty?
       self.procedure_categories.first.brand if self.procedure_categories.first.brand
