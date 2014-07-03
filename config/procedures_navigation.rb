df67e67aadaa8601094d3b09b3dfbc6cae066ab5
justvitalius <justvitalius@gmail.com>
2014-07-03 22:39:29 +0400

    Render procedures left menu.

--- config/procedures_navigation.rb
+++ config/procedures_navigation.rb
@@ -12,93 +12,21 @@ SimpleNavigation::Configuration.run do |navigation|
   # If this option is set to true, all item names will be considered as safe (passed through html_safe). Defaults to false.
   navigation.consider_item_names_as_safe = true
 
-  navigation.items do |taxonomies|
-    taxonomies.dom_class = '_left-menu-root-level _left-menu js-left-menu js-left-menu-node'
-    Taxonomy.order('title ASC').each do |taxonomy|
-      taxonomies.item "taxonomy-#{taxonomy.id}", taxonomy.title, taxonomy_path(taxonomy.slug) do |taxons|
-        taxons.dom_class = '_left-menu-first-level js-left-menu-node'
-        taxonomy.taxons.roots.each do |taxon|
-          taxons.item "taxon-#{taxon.id}", taxon.title, taxon_seo_path(taxon) do |subtaxons|
-            subtaxons.dom_class = '_left-menu-second-level js-left-menu-node'
-            taxon.children.each do |subtaxon|
-              subtaxons.item "taxon-#{subtaxon.id}", subtaxon.title, taxon_seo_path(subtaxon), class: 'empty'
+  navigation.items do |procedure_categories|
+    procedure_categories.dom_class = '_left-menu-root-level _left-menu js-left-menu js-left-menu-node'
+    Brand.order('title ASC').each do |brand|
+      procedure_categories.item "brand-#{brand.id}", brand.title, '#' do |brand_procedure_categories|
+        brand_procedure_categories.dom_class = '_left-menu-first-level js-left-menu-node'
+        brand.procedure_categories.roots.each do |procedure_category_root|
+          brand_procedure_categories.item "procedure-category-#{procedure_category_root.id}", procedure_category_root.title, '#' do |procedure_categories_child|
+            procedure_categories_child.dom_class = '_left-menu-second-level js-left-menu-node'
+            procedure_category_root.children.each do |procedure_category_child|
+              procedure_categories_child.item "procedure-category-#{procedure_category_child.id}", procedure_category_child.title, '#', class: 'empty'
             end
           end
         end
       end
     end
   end
-  # Specify a custom renderer if needed.
-  # The default renderer is SimpleNavigation::Renderer::List which renders HTML lists.
-  # The renderer can also be specified as option in the render_navigation call.
-  # navigation.renderer = Your::Custom::Renderer
-
-  # Specify the class that will be applied to active navigation items.
-  # Defaults to 'selected' navigation.selected_class = 'your_selected_class'
-
-  # Specify the class that will be applied to the current leaf of
-  # active navigation items. Defaults to 'simple-navigation-active-leaf'
-  # navigation.active_leaf_class = 'your_active_leaf_class'
-
-  # Item keys are normally added to list items as id.
-  # This setting turns that off
-  # navigation.autogenerate_item_ids = false
-
-  # You can override the default logic that is used to autogenerate the item ids.
-  # To do this, define a Proc which takes the key of the current item as argument.
-  # The example below would add a prefix to each key.
-  # navigation.id_generator = Proc.new {|key| "my-prefix-#{key}"}
-
-  # If you need to add custom html around item names, you can define a proc that
-  # will be called with the name you pass in to the navigation.
-  # The example below shows how to wrap items spans.
-  # navigation.name_generator = Proc.new {|name, item| "<span>#{name}</span>"}
-
-  # The auto highlight feature is turned on by default.
-  # This turns it off globally (for the whole plugin)
-  # navigation.auto_highlight = false
-
-  # If this option is set to true, all item names will be considered as safe (passed through html_safe). Defaults to false.
-  # navigation.consider_item_names_as_safe = false
-
-  # Define the primary navigation
-  #navigation.items do |primary|
-    # Add an item to the primary navigation. The following params apply:
-    # key - a symbol which uniquely defines your navigation item in the scope of the primary_navigation
-    # name - will be displayed in the rendered navigation. This can also be a call to your I18n-framework.
-    # url - the address that the generated item links to. You can also use url_helpers (named routes, restful routes helper, url_for etc.)
-    # options - can be used to specify attributes that will be included in the rendered navigation item (e.g. id, class etc.)
-    #           some special options that can be set:
-    #           :if - Specifies a proc to call to determine if the item should
-    #                 be rendered (e.g. <tt>if: -> { current_user.admin? }</tt>). The
-    #                 proc should evaluate to a true or false value and is evaluated in the context of the view.
-    #           :unless - Specifies a proc to call to determine if the item should not
-    #                     be rendered (e.g. <tt>unless: -> { current_user.admin? }</tt>). The
-    #                     proc should evaluate to a true or false value and is evaluated in the context of the view.
-    #           :method - Specifies the http-method for the generated link - default is :get.
-    #           :highlights_on - if autohighlighting is turned off and/or you want to explicitly specify
-    #                            when the item should be highlighted, you can set a regexp which is matched
-    #                            against the current URI.  You may also use a proc, or the symbol <tt>:subpath</tt>.
-    #
-    #primary.item :key_1, 'name', url, options
-
-    # Add an item which has a sub navigation (same params, but with block)
-    #primary.item :key_2, 'name', url, options do |sub_nav|
-      # Add an item to the sub navigation (same params again)
-      #sub_nav.item :key_2_1, 'name', url, options
-    #end
-
-    # You can also specify a condition-proc that needs to be fullfilled to display an item.
-    # Conditions are part of the options. They are evaluated in the context of the views,
-    # thus you can use all the methods and vars you have available in the views.
-    #primary.item :key_3, 'Admin', url, class: 'special', if: -> { current_user.admin? }
-    #primary.item :key_4, 'Account', url, unless: -> { logged_in? }
-
-    # you can also specify html attributes to attach to this particular level
-    # works for all levels of the menu
-    # primary.dom_attributes = {id: 'menu-id', class: 'menu-class'}
-
-    # You can turn off auto highlighting for a specific level
-    # primary.auto_highlight = false
-  #end
+  
 end
