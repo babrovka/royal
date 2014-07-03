b9c6ae1f245217329e609ca3f638e686f1b50966
babrovka <babrovka@gmail.com>
2014-07-03 11:08:33 +0400

    brand method for procedure

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -4,4 +4,19 @@ class Procedure < ActiveRecord::Base
   accepts_nested_attributes_for :stages, :allow_destroy => true
   has_attached_file :image, :styles => { :medium => "940x380#", :thumb => "200x100>" }
   
+  
+  def brand 
+    if self.stages
+      stages = self.stages.pluck(:id)
+      substages = Substage.where(:stage_id => stages)
+      if substages
+        products = Product.includes(:substages).where("substages.id" => substages) 
+        unless products.empty? && products.first.brand
+          brand = products.first.brand
+          brand
+        end
+      end
+    end
+  end
+  
 end
