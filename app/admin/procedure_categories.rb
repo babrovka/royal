614b4ed4145a7f2f6e72c1887712c4cd53dba3a3
babrovka <babrovka@gmail.com>
2014-07-07 15:52:58 +0400

    procedure category admin

---
+++ app/admin/procedure_categories.rb
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+ActiveAdmin.register ProcedureCategory do
+  menu :parent => I18n.t('catalog') 
+  config.batch_actions = false
+  config.clear_sidebar_sections!
+  
+  
+   index do 
+     column :brand_id do |column|
+       if column.brand_id 
+         Brand.find(column.brand_id).try(:title)
+       end
+     end
+     column :title
+     
+     default_actions
+   end
+
+   form do |f|  
+     f.inputs do
+       f.input :brand_id, :as => :select, :collection => Brand.all
+       f.input :parent_id, :as => :select, :collection => ProcedureCategory.where(:depth => 0)
+       f.input :title
+     end
+     f.actions
+   end
+
+  show do |category|
+    attributes_table do
+      row :brand_id
+      row :title
+      row :parent_id
+    end  
+    
+    panel t('procedures') do 
+      table_for category.procedures do 
+        column 'Название' do |column|
+          column.title
+        end
+      end
+    end
+   end
+end
--- app/models/procedure_category.rb
+++ app/models/procedure_category.rb
@@ -1,5 +1,5 @@
 class ProcedureCategory < ActiveRecord::Base
-  attr_accessible :title, :brand_id
+  attr_accessible :title, :brand_id, :parent_id
   has_many :procedure_procedure_categories
   has_many :procedures, through: :procedure_procedure_categories
   acts_as_nested_set
