f01660376ce8234c7479597b44a676515f87ff81
babrovka <babrovka@gmail.com>
2014-07-11 14:12:40 +0400

    redcarpet on procedure show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -70,6 +70,11 @@ module ApplicationHelper
      Redcarpet.new(text, :hard_wrap).to_html.html_safe
    end
    
+   def special_markdown(number, text)
+     text = number.to_s + '.&nbsp;' + text 
+     Redcarpet.new(text, :hard_wrap).to_html.html_safe
+   end
+   
    def title
       base_title = "Royal Beauty"
       if @title
