ff2bcac06121b50f3b56d1a82b5cf43cd30ebf1c
babrovka <babrovka@gmail.com>
2014-08-14 18:46:21 +0400

    croper for all product images

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -4,10 +4,10 @@ class ProductImage < ActiveRecord::Base
   belongs_to :product
   
   has_attached_file :image, styles: { 
-                                        catalog: {geometry: "200x200#", processors: [:cropper]},
+                                        catalog: {geometry: "200x200#"},
                                         product: {geometry: "300x300#"},
                                         ico: {geometry: "70x70#"}
-  }
+                                     }, processors: [:cropper]
   
   after_update :reprocess_image, :if => :cropping? 
   
